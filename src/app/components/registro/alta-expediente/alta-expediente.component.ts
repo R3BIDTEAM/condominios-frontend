dd0da8fb7bb4d5278f09031e2f5ada171c76024a
mdperezr13 <mdperezr13@gmail.com>
2021-08-03 22:46:53 -0500

    cambio fechas

--- src/app/components/registro/alta-expediente/alta-expediente.component.ts
+++ src/app/components/registro/alta-expediente/alta-expediente.component.ts
@@ -5,34 +5,40 @@ import { environment } from '@env/environment';
 import { AuthService } from '@serv/auth.service';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
+import { DatePipe } from '@angular/common';
 
 export interface DataExpediente {
   IDTIPOTRAMITE: string;
-  FECHAENTRADA: Date;
-  FECHATERMINO: Date;
+  FECHAENTRADA: string;
+  FECHATERMINO: string;
   OBSERVACIONES: string;
 }
 @Component({
   selector: 'app-alta-expediente',
   templateUrl: './alta-expediente.component.html',
-  styleUrls: ['./alta-expediente.component.css']
+  styleUrls: ['./alta-expediente.component.css'],
+  providers: [DatePipe]
 })
 export class AltaExpedienteComponent implements OnInit {
   loadingTiposTramite = false;
   tiposTramite;
   documentosAportar = [1,1,1,1];
   documentosAportarColumns: string[] = ['conjunto_documental', 'documento', 'obligatorio', 'check'];
+  hoy = new Date();
   dataExpediente: DataExpediente = {} as DataExpediente;
 
   constructor(
     private http: HttpClient,
     private snackBar: MatSnackBar,
+    private datePipe: DatePipe
   ) { }
 
   ngOnInit(): void {
     this.getTiposTramite();
     this.dataExpediente.IDTIPOTRAMITE = "";
-    this.dataExpediente.FECHAENTRADA = new Date((new Date().getTime()));
+    this.dataExpediente.FECHAENTRADA = "FEC_" + this.datePipe.transform(this.hoy, 'dd/MM/yyyy');
+    this.dataExpediente.FECHATERMINO = "FEC_" + this.datePipe.transform(this.hoy, 'dd/MM/yyyy');
+    console.log(this.dataExpediente);
   }
 
   getTiposTramite(): void {
