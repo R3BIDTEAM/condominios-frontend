231556275ab48144744363b4a28c2d543bb503c7
mdperezr13 <mdperezr13@gmail.com>
2021-08-11 00:41:55 -0500

    enviroment qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -4,10 +4,14 @@
 
 export const environment = {
     production: false,
-    appName: 'base',
-    endpoint: '',
-    ssoEndpoint: '',
-    ssoToken: '',
+    closeSession: true,
+    baseHref: '/',
+    appName: 'ADYCON',
+    endpoint: 'https://rest-adycon-dot-build-bpm.uc.r.appspot.com/v1/adycon',
+    ssoEndpoint: 'http://ovica.linesolutions.tech/sso-backend/public/api/v1/',
+    ssoRemember: 'http://ovica.linesolutions.tech/sso/recordar-password',
+    rconEndpoint: 'http://ovica.linesolutions.tech/rcon-backend/public/api/v1/',
+    ssoToken: 'eyJpdiI6ImhpdHI3YU53OVlGd3hXSDM1RWRZOEE9PSIsInZhbHVlIjoiUklZSkJQMHJMcUNEWXVhVEpET085eGd2bHBmZzN5K0hoandTSGluVUp5VW1NZmtZOW9LZkxiRnVkbjk5RE5sSzY5elJqVHJnbmJaYVh6d1ExaFIvcEE3K3pMdTBMQWwvL09UeEVLNHV5ZmhWc1NBbysza1U3cnFySnVTNVMzT0QzeFVlZFhtWVo2V1UvYlE0MitqSWc4WmdZVG55U0JKZ2ExbFIvcGg4M3NzamtnMXZXNFQxR0FVZWNVWFhYL0VLMXJkL0ZGUGt5MFRMK3NQTUYwdWp6dFVKbXh6Mmh0V1dKbGg4ay9Rc1pOZmJVSjIxYWM3S1lmNTVkcVJST2tpOHRzaWxpbVc5QjZpUkhmUWJydFFMWEM5U0xFV082NTJWNGwrcE16WjUvWmtuYlo5SVBaQ1hCTjg3QW12Nkh0V2NrelBibmRSV2Y2NCtUTzdyOTZoK3NRdDBQM0lIdDdndldMOEZxbmlxUWdsVnluVlNvL2xwUHRYMHhZb3lCY05lY1loZDFWZXczVFJTNk1RMzZGT0FZUDJ5Q1BwOGkwTmJXemRLaktLdGc1a1hrOW4rUlBEejBmdk5hNVBGeTgwWVlSMFZPL2hCRGloaE1IWG41bVpnRGc9PSIsIm1hYyI6ImM0MDM0NWVmZmQ4ODZmNzhkZTExYjNiZGExZGFkNGViMzUzYzlkNDllOTU1NTNiZWYwOWYyYzhhMTU1NDczMGMifQ==',
 };
 
 /*
